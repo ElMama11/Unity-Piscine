6d1e981a2f9cbaa0f5f73d2b53dc66c671b03df5
Maxence <verger.maxence42@gmail.com>
2024-03-12 02:26:59 +0100

    wip ex02

--- unityModule03/Assets/Content/Scripts/DragDrop.cs
+++ unityModule03/Assets/Content/Scripts/DragDrop.cs
@@ -3,25 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler {
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler {
     private RectTransform rectTransform;
+	private CanvasGroup canvasGroup;
+	[SerializeField] private Canvas canvas;
 
     private void Awake() {
         rectTransform = GetComponent<RectTransform>();
+		canvasGroup = GetComponent<CanvasGroup>();
     }
     public void OnPointerDown(PointerEventData eventData) {
-        Debug.Log("ONPOINTER DOWN");
     }
 
     public void OnBeginDrag(PointerEventData eventData) {
-        Debug.Log("OnBeginDrag");
+        // Debug.Log("OnBeginDrag");
+		canvasGroup.alpha = 0.6f;
+		canvasGroup.blocksRaycasts = false;
     }
 
     public void OnEndDrag(PointerEventData eventData) {
-        Debug.Log("OnEndDrag");
+		canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
     }
     public void OnDrag(PointerEventData eventData) {
-        rectTransform.anchoredPosition += eventData.delta;
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+	public void OnDrop(PointerEventData eventData) {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
 }
--- unityModule03/Assets/Content/Scripts/Plot.cs
+++ unityModule03/Assets/Content/Scripts/Plot.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Plot : MonoBehaviour
+public class Plot : MonoBehaviour, IDropHandler
 {
 	[Header("References")]
 	[SerializeField] private SpriteRenderer _sr;
@@ -22,8 +23,21 @@ public class Plot : MonoBehaviour
 		_sr.color = _startColor;
 	}
 
-	private void OnMouseDown() {
-		if (_tower != null)
+	// private void OnMouseDown() {
+	// 	if (_tower != null)
+	// 		return;
+	// 	Tower tmpTower = BuildManager.main.getSelectedTower();
+	// 	if (tmpTower.cost > GameManager.Instance.currency) {
+	// 		Debug.Log("You can't buy that");
+	// 		return;
+	// 	}
+	// 	GameManager.Instance.spendCurrency(tmpTower.cost);
+	// 	_tower = Instantiate(tmpTower.prefab, transform.position, Quaternion.identity);
+	// }
+
+	public void OnDrop(PointerEventData eventData) {
+		Debug.Log("YEP");
+	if (_tower != null)
 			return;
 		Tower tmpTower = BuildManager.main.getSelectedTower();
 		if (tmpTower.cost > GameManager.Instance.currency) {
