9a372d84b8012daf13c38bfa2e112363ba3384ff
ElMama11 <mverger@z3r2p5.42lyon.fr>
2024-03-15 19:15:44 +0100

    f

---
+++ unityModule05/Assets/Content/Script/UI/BtnMainMenu.cs
@@ -0,0 +1,9 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BtnMainMenu : MonoBehaviour {
+    public void BtnMain() {
+        Debug.Log("ddddddddddddd");
+        SceneManager.LoadScene(0);
+    }
+}
